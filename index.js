7fd671d253928487f26c5b65ac21ae92fc551843
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2024-01-19 21:16:03 +0300

    aster

--- index.js
+++ index.js
@@ -1,87 +1,107 @@
 
-let myObj=[
-    [
-    [firstName ,""],
-    [familyName ,""],
-    [title ,""],
-    [payPerHour ,""]
-    
-    ]
+let myObj=
+[
+    {firstName :""},
+    {familyName :""},
+    {title:""},
+    {payPerHour:""}
+]
  // {  timeInEvents : [""] }
  // {  timeOutEvents :[""]}
-]
-
-function createEmployeeRecord(myObj){ 
-    return `${myObj.firstName} ${myObj.familyName}  ${myObj.title}
-     ${myObj.payPerHour} ` 
-     //const phrase=
-     [
-        [timeInEvents,""],
-        [timeOutEvents,""]
-     ]
+function createEmployeeRecord([anArray]){ 
+    let myObj=
+    [
+        {firstName :anArray[0]},
+        {familyName :anArray[1]},
+        {title:[2]},
+        {payPerHour:[3]},
+        {timeInEvents:""},
+        {timeOutEvents:""}
+    ]
+    return myObj
+    
+  //  return `${myObj.firstName} ${myObj.familyName}  ${myObj.title}
+  //   ${myObj.payPerHour} ${phrase}` 
+     //const phrase=    
 }
-createEmployeeRecord.apply(myObj,phrase)
+createEmployeeRecord()
+
+
 
 /*employee2(timeInEvents)
 let employee3=createEmployeeRecord.bind(myObj)
 employee3(timeOutEvents)*/
-function createEmployeeRecords(line){
-return `${this.firstName} ${this.familyName} ${this.title} ${this.payPerHour} ${line}`
+function createEmployeeRecords(theArray){
+//return `${this.firstName} ${this.familyName} ${this.title} ${this.payPerHour} ${line}`
+createEmployeeRecord(myObj)
+
+/*for(let i=0;i<myObj.length;i++){
+    return i
 }
-const line2=[
-[timeInEvents,""],
-[timeOutEvents,""]
-]
-createEmployeeRecords.bind(myObj)
+let allMyEmployees=[]
+allMyEmployees.push(myObj[i])
+return allMyEmployees[i]*/
+return theArray.map(function(q){
+    return createEmployeeRecord(q)
+    
+})
+}
+
+
+
 //let allEmployees=createEmployeeRecord.apply(myObj,myObj[1],myObj[2])
 //allEmployees(timeInEvents)
 //let allEmployees2=createEmployeeRecord.bind(myObj)
 //allEmployees2(timeOutEvents)
-const date =new Date()
+const date1 =new Date()
 function createTimeInEvent(date){   
 //const myDate=date.getHours()
 //const date2=date.getDate()
-const myDate=date.getHours()
-const date2=date.getDate()
-const myObj2=[timeInEvents,`${date2}`]
 return `${this.firstName} ${this.familyName}  ${this.title}
-     ${this.payPerHour} ${myObj2} `
-     
+     ${this.payPerHour} ${date} `
+    
 }
-const myDate=date.getHours()
-const date2=date.getDate()
-createTimeInEvent.call(myObj,myObj2)
+const myDate=date1.getHours()
+const date2=date1.getDate()
+/*const myObj2=[
+    {timeIn:`${myDate}`},
+    {hour:`${myDate}`},
+    {date5:`${date2}`}
+]*/
+phrase[0]=`${myDate}`
+createTimeInEvent.call(myObj,phrase[0])
+
 function createTimeOutEvent(date){
-const date3=date.getHours()
-const date4=date.getDate()
-const myObj3=[timeOutEvents,`${date4}`]
+//const myObj3={timeOutEvents:`${date4}`}
 return `${this.firstName} ${this.familyName}  ${this.title}
-     ${this.payPerHour} ${myObj3} `  
-     
+     ${this.payPerHour} ${date} `     
 }
-createTimeOutEvent.call(myObj,myObj3)
-function hoursWorkedOnDate(date){
-const myObj4=[timeInEvents,date]
-const myObj5=[timeOutEvents,date]
- const hrsElapsed=`${myObj5}`-`${myObj4}`
+//const myObj3=[[type ,"timeOut"],[hour,`${myDate}`],[date,`${date2}`]]
+phrase[1]=`${myDate}`
+createTimeOutEvent.call(myObj,phrase[1])
+function hoursWorkedOnDate(date1){
+const myObj4=phrase[1]
+const myObj5=phrase[0]
+ const hrsElapsed=`${myObj4}`-`${myObj5}`
  return hrsElapsed
     }
    function hoursWorkedOnDate(date){
-    const myObj6=[timeInEvents,date]
-    const myObj7=[timeOutEvents,date]
+    const myObj6=date1.getDate()
+    const myObj7=date1.getDate()
     const hrsWorked=`${myObj6}`-`${myObj7}`
     return hrsWorked
    }
    let myEmployees=[]
-   function findEmployeeByFirstName(myEmployees,name){
-      if(myEmployees===name){
-         return `${myObj}`
+   function findEmployeeByFirstName(name){
+      if(`${this.firstName===name}`){
+         return myObj
    }
    else{
     return "undefined"
    }
    }
-   findEmployeeByFirstName.bind(myEmployees,`${myObj.firstName}`)
+   findEmployeeByFirstName.call(myObj,`${myObj.firstName}`)
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
